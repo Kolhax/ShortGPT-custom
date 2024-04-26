622ef64160675fa54feeb4f66710bfd6f4521b0d
Kolhax <92904192+Kolhax@users.noreply.github.com>
2024-04-26 11:40:36 -0400

    a

--- shortGPT/gpt/gpt_utils.py
+++ shortGPT/gpt/gpt_utils.py
@@ -71,7 +71,7 @@ def open_file(filepath):
 
 def gpt3Turbo_completion(chat_prompt="", system="You are an AI that can give the answer to anything", temp=0.7, model="gpt-3.5-turbo", max_tokens=1000, remove_nl=True, conversation=None):
     openai.api_key = ApiKeyManager.get_api_key("OPENAI")
-    openai.base_url = "http://localhost:3040/v1/"
+    openai.base_url = "http://192.168.0.102:3040/v1/"
     max_retry = 5
     retry = 0
     while True:
